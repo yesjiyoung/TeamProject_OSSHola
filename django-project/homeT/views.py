cf49a4131caf9ebf6a6a77d44874419db69e28ff
Hyeseon Lee <hyesun1999@gmail.com>
2020-06-22 02:38:52 +0900

    [Back-end] feat: VideoLoad function

--- django-project/homeT/views.py
+++ django-project/homeT/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+class VideoLoad(ListView):
+    model = Video
+
+    def get(self,reqest):
+        template_name = 'home_be.html'
+        VideoList = Video.objects.all()
+        return render(request, template_name, {'VideoList':VideoList})
