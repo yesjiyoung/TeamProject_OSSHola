f548afb4ebd5b804db1b6fd46147eda9f5abb83b
Hyeseon Lee <hyesun1999@gmail.com>
2020-06-22 02:07:10 +0900

    [Back-end] feat: Edit Video model

--- django-project/homeT/models.py
+++ django-project/homeT/models.py
@@ -1,25 +1,31 @@
 from django.db import models
-from django.shortcuts import resolve_url
+# from django.shortcuts import resolve_url
 
 # Create your models here.
-class Category(models.Model):
-    parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True, related_name='sub_categories')
-    name = models.CharField(max_length=200) # category 이름
-    description = models.TextField(max_length=200) # 간단 설명
-    slug = models.SlugField(max_length=200, allow_unicode=True, unique=True)
-    def __str__(self):
-        return self.name
-    class Meta:
-        ordering=['name']
-    def get_absolute_url(self):
-        return resolve_url('product_in_category', self.slug)
+# class Category(models.Model):
+#     parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True, related_name='sub_categories')
+#     name = models.CharField(max_length=200) # category 이름
+#     description = models.TextField(max_length=200) # 간단 설명
+#     slug = models.SlugField(max_length=200, allow_unicode=True, unique=True)
+#     def __str__(self):
+#         return self.name
+#     class Meta:
+#         ordering=['name']
+#     def get_absolute_url(self):
+#         return resolve_url('product_in_category', self.slug)
         
 class Video(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name='videos')
-    name = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to='video_thumbnails/%Y/%m/%d')
-    created = models.DateTimeField(auto_now_add=True)
-    slug = models.SlugField(max_length=200, allow_unicode=True, unique=True)
+    title = models.CharField(max_length=200)
+    # description = models.TextField(blank=True, null=True)
+    # image = models.ImageField(upload_to='video_thumbnails/%Y/%m/%d')
+    v_id = models.CharField(max_length=140, help_text="youtube 영상 코드를 입력해주세요")
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+    class Meta:
+        ordering=['-created_at']
+
     def __str__(self):
-        return "["+self.category.name+"]"+self.name
+        return self.title
+
+    
+
