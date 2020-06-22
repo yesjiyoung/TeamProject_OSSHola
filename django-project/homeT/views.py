a1f34fd2240c44e0061bea41a07c3f667cd85fae
yesjiyoung <rohjy77@gmail.com>
2020-06-22 12:28:09 +0900

    [Back-end] feat: Add signup.html url view

--- django-project/homeT/views.py
+++ django-project/homeT/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Video
+
+#[JY]About User 
+from django.contrib.auth.models import User  
+from django.contrib import auth  
+
 # Create your views here.
+
 
 
 class VideoLoad(ListView):
@@ -11,3 +17,19 @@ class VideoLoad(ListView):
         template_name = 'home_be.html'
         VideoList = Video.objects.all()
         return render(request, template_name, {'VideoList':VideoList})
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+            auth.login(request, user)
+            return redirect('correct')
+        else:
+            return redirect('uncorrect')
+    return render(request, 'signup.html')
+
+
+
+
