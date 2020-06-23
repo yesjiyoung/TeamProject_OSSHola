6d44e8ce3ae71c6018475383bcfbbb2d95158572
yesjiyoung <rohjy77@gmail.com>
2020-06-23 19:51:30 +0900

    [Back-end] fix : Merge coflict

--- django-project/homeT/views.py
+++ django-project/homeT/views.py
@@ -3,7 +3,7 @@ from django.views.generic.list import ListView
 from .models import Video
 
 #[JY]About User 
-from django.contrib.auth.models import User  
+from djaontrib.auth.models import User  
 from django.contrib import auth  
 
 # Create your views here.
@@ -24,7 +24,7 @@ def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
-            #auth.login(request, user)
+            auth.login(request, user)
             return redirect('correct')
         else:
             return redirect('uncorrect')
