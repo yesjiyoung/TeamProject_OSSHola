0e52069ecf113f41ba543055114b07f51ba187f5
yesjiyoung <rohjy77@gmail.com>
2020-06-23 20:36:46 +0900

    [Back-end] fix : Resolved #issue34 login bug

--- django-project/homeT/views.py
+++ django-project/homeT/views.py
@@ -65,7 +65,7 @@ def login(request):
         user = auth.authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            return redirect('home')
+            return redirect('main')
         else:
             return render(request, 'unlog_be.html')
     else:
@@ -79,6 +79,10 @@ def logout(request):
     if request.method == 'POST':
         auth.logout(request)
         return redirect('home')
+    return render(request, 'home.html')
+
+
+def home(request):
     return render(request, 'home.html')
 
 
