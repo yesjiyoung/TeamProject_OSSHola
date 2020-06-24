656a5deea6a2f34626713173473621713e728042
yesjiyoung <rohjy77@gmail.com>
2020-06-24 20:10:12 +0900

    [Back-end] feat : Create Comment Model, view

--- django-project/homeT/admin.py
+++ django-project/homeT/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-#[JY] User Model 작업때문에 잠시 주석처리합니다.-> 다시 살림
-from .models import Video, Like
+
+from .models import Video, Like, Comment
 
 # Register your models here.
 admin.site.register(Video)
+admin.site.register(Comment)
 
 from .models import User
 from django.contrib.auth.admin import UserAdmin
--- django-project/homeT/models.py
+++ django-project/homeT/models.py
@@ -69,3 +69,13 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,  on_delete=models.CASCADE, blank=True, related_name='comments')
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=160)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}.{}'.format(self.video.title, str(self.user.username))
