70e7b1f504204a8cae9c0090dbf1ba9de261ad94
Hyeseon Lee <hyesun1999@gmail.com>
2020-06-25 01:47:47 +0900

    [Front-end] feat : main page layout finish

--- django-project/homeT/views.py
+++ django-project/homeT/views.py
@@ -60,7 +60,7 @@ def detail(request, detail_id):
 
 def ai_detail(request, ai_detail_id):
     detail_ai_obj = get_object_or_404(Video, pk=ai_detail_id)
-    return render(request, 'Ai_detail.html', {'detail_ai_obj':detail_ai_obj})
+    return render(request, 'ai_detail.html', {'detail_ai_obj':detail_ai_obj})
 
 @require_POST
 def video_like(request):
